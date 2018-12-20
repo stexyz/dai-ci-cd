2e38aab2c66f46ff6fa0fe1c4fea2d35149b5d4f
stexyz <pacinda@seznam.cz>
2018-12-20 14:28:53 +0100

    Colorful logs.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,6 +12,7 @@ pipeline {
 
     // Setup job options.
     options {
+        ansiColor('xterm')
         timestamps()
         timeout(time: 60, unit: 'MINUTES')
         // holding last 10 builds
