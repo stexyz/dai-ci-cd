c018fe6245c14ba6bb1052051035cf6be46aa075
stexyz <pacinda@seznam.cz>
2019-01-15 23:35:16 +0100

    Finished deploy_mojo.

--- deploy_mojo.py
+++ deploy_mojo.py
@@ -15,15 +15,11 @@ MOJO_PATH = sys.argv[4]
 MODEL_BUCKET = sys.argv[5]
 MOJO_OBJECT_NAME = sys.argv[6]
 
-s3 = boto3.client('s3',
+r3 = boto3.resource('s3',
                   endpoint_url=MINIO_URL,
                   aws_access_key_id=MINIO_ACCESS_KEY,
                   aws_secret_access_key=MINIO_ACCESS_KEY,
                   region_name='us-east-1')
-
-
-# 1. remove the old mojo
-# 2. upload new mojo
 
 # Clearly for proper production use we would use some tagging to only keep adding new mojo files 
 # and our production app would select the latest mojo file to run the predictions; 
@@ -33,9 +29,9 @@ s3 = boto3.client('s3',
 # https://stackoverflow.com/questions/40336918/how-to-write-a-file-or-data-to-an-s3-object-using-boto3
 # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/migrations3.html
 
-s3.Object('mybucket', MOJO_OBJECT_NAME).put(Body=open('/tmp/hello.txt', 'rb'))
+# Rewriting the old mojo for now
+# https://stackoverflow.com/questions/23764683/are-writes-to-amazon-s3-atomic-all-or-nothing
 
-s3.
-
+r3.Object(MODEL_BUCKET, MOJO_OBJECT_NAME).put(Body=open(MOJO_PATH, 'rb'))
 
 print('success')
