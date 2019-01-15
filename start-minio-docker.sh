41937f93e803af904477089c2405b87f57614ab2
stexyz <pacinda@seznam.cz>
2019-01-15 17:05:33 +0100

    Added minio docker startup script.

--- start-minio-docker.sh
+++ start-minio-docker.sh
@@ -1,2 +1,8 @@
 docker pull minio/minio
-docker run -p 9000:9000 minio/minio server /data
+docker run -p 9000:9000 \
+  -v "`pwd`/minio/data":"/data" \
+  -v "`pwd`/minio/config":"/root/.minio" \
+  -e "MINIO_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE" \
+  -e "MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" \
+  --name minio \
+  minio/minio server /data
