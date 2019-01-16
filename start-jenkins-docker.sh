8a4fd765ecbd053ee140cd7229ac13bd40c65be5
stexyz <pacinda@seznam.cz>
2019-01-16 01:12:28 +0100

    Reverted Tunneling port 9000 for Minio access from Jenkins.

--- start-jenkins-docker.sh
+++ start-jenkins-docker.sh
@@ -1,3 +1,3 @@
-docker run --name dai-ci-cd-jenkins --rm -p 8080:8080 -p 50000:50000 -p 9000:9000 -v "/tmp/jenkins_home/":"/var/jenkins_home" jenkins/jenkins:lts
+docker run --name dai-ci-cd-jenkins --rm -p 8080:8080 -p 50000:50000 -v "/tmp/jenkins_home/":"/var/jenkins_home" jenkins/jenkins:lts
 
 # check if --rm should be kept after debugging
