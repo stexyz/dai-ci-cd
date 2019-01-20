239205ed126884a16329ade12d347ff34ddd86dd
stexyz <pacinda@seznam.cz>
2019-01-20 16:23:09 +0100

    Jenkins image version update.

--- start-jenkins-docker.sh
+++ start-jenkins-docker.sh
@@ -1,3 +1,3 @@
-docker run --name dai-ci-cd-jenkins --rm -p 8080:8080 -p 50000:50000 -v "/tmp/jenkins_home/":"/var/jenkins_home" jenkins/jenkins:lts
+docker run --name dai-ci-cd-jenkins --rm -p 8080:8080 -p 50000:50000 -v "/tmp/jenkins_home/":"/var/jenkins_home" jenkins/h2o:v2
 
 # check if --rm should be kept after debugging
