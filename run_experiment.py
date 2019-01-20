36aed47e3c3d80eb99258a994fa2891d72c0766a
stexyz <pacinda@seznam.cz>
2019-01-20 16:48:18 +0100

    Experiment to run on Jenkins changet to TS on Walmart.

--- run_experiment.py
+++ run_experiment.py
@@ -16,13 +16,20 @@ target = 'DEFAULT_PAYMENT_NEXT_MONTH'
 # print("About to start experiment with accuracy=[", sys.argv[2], "], time=[", sys.argv[3], "], interpretability=[",sys.argv[4],"].")
 # start experiment synchronously
 experiment = h2oai.start_experiment_sync(dataset_key = sys.argv[4],
-                                         # testset_key = test.key,
-                                         target_col = target,
-                                         is_classification = True,
+                                         target_col = "Weekly_Sales",
+                                         orig_time_col = 'Date',
+                                         time_col = 'Date',
+                                         is_classification = False,
+                                         enable_gpus = True,
                                          accuracy = int(sys.argv[5]),
                                          time = int(sys.argv[6]),
                                          interpretability = int(sys.argv[7]),
-                                         scorer = "AUC",
+                                         scorer = 'RMSE',
+                                         time_groups_columns = ['Date', 'Dept', 'Store'],
+                                         time_period_in_seconds = 604800,
+                                         num_prediction_periods = 1,
+                                         num_gap_periods = 0,
+                                         is_timeseries = True,
                                          seed = 1234)
 
 # output the key of the experiment so that next build step in Jenkins can use it
